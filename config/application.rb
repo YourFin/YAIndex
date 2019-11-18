08927168a289dbf3a27b6941e3974e665d40aef4
YourFin <d.junkpobox+git@gmail.com>
2019-11-17 20:56:53 -0800

    Disable unwanted generators

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Researcher
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.stylesheets false
+      g.assets false
+    end
   end
 end
