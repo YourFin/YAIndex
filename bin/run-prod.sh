0e5bd18b3f8c3cd76ee3eca4adddfef91405bc0a
YourFin <d.junkpobox+git@gmail.com>
2019-12-04 21:37:55 -0800

    Add nginx templating

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 # Make sure pids folder exists
 mkdir -p tmp/pids
 
@@ -5,7 +8,21 @@ mkdir -p tmp/pids
 rm -f tmp/pids/server.pid
 rm -f /run/nginx.pid
 
+# Make sure PORT and FILES_DIRECTORY are set
+PORT="${PORT:-80}"
+FILES_DIRECTORY="${FILES_DIRECTORY:-/app/test_files/}"
+
+# Substitute environment variables into nginx.conf
+envsubst '$PORT $FILES_DIRECTORY' < nginx/nginx.template.conf > nginx/nginx.conf
+
 # Start nginx
-nginx
+nginx -c nginx/nginx.conf
+
+# Kill nginx when this script stops
+cleanup () {
+    nginx -c nginx/nginx.conf -s quit
+}
+trap cleanup EXIT
+
 # Start puma
 bundle exec puma -b "tcp://localhost" -C /app/config/puma.rb
