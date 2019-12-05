82394469354c446fdff77a7a45192b591a407a4c
YourFin <d.junkpobox+git@gmail.com>
2019-12-04 23:21:09 -0800

    Automatically append slashes to FILES_DIRECTORY for nginx

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -12,6 +12,11 @@ rm -f /run/nginx.pid
 export PORT="${PORT:-80}"
 export FILES_DIRECTORY="${FILES_DIRECTORY:-/app/test_files/}"
 
+# append a trailing slash if need be
+if [[ "${FILES_DIRECTORY: -1}" != "/" ]] ; then
+    export FILES_DIRECTORY="$FILES_DIRECTORY/"
+fi
+
 # Substitute environment variables into nginx.conf
 envsubst '$PORT $FILES_DIRECTORY' < nginx/nginx.template.conf > nginx/nginx.conf
 
