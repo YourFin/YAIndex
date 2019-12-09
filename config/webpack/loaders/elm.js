ed8f61c9fd4699a665fcd2fb9974ad130478434d
YourFin <d.junkpobox+git@gmail.com>
2019-12-08 22:23:37 -0800

    Add optimize flag to elm in production
    Also adds some semicolons

--- config/webpack/loaders/elm.js
+++ config/webpack/loaders/elm.js
@@ -1,22 +1,25 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production'
-const elmSource = resolve(process.cwd())
-const elmBinary = `${elmSource}/node_modules/.bin/elm`
+const isProduction = process.env.NODE_ENV === 'production';
+const elmSource = resolve(process.cwd());
+const elmBinary = `${elmSource}/node_modules/.bin/elm`;
 
-const elmDefaultOptions = { cwd: elmSource, pathToElm: elmBinary }
+const elmDefaultOptions = { cwd: elmSource, pathToElm: elmBinary };
+const productionOptions = Object.assign({}, elmDefaultOptions, {
+    optimize: true
+});
 const developmentOptions = Object.assign({}, elmDefaultOptions, {
   verbose: true,
   debug: true
-})
+});
 
 const elmWebpackLoader = {
   loader: 'elm-webpack-loader',
-  options: isProduction ? elmDefaultOptions : developmentOptions
-}
+  options: isProduction ? productionOptions : developmentOptions
+};
 
 module.exports = {
   test: /\.elm(\.erb)?$/,
   exclude: [/elm-stuff/, /node_modules/],
   use: isProduction ? [elmWebpackLoader] : [{ loader: 'elm-hot-webpack-loader' }, elmWebpackLoader]
-}
+};
