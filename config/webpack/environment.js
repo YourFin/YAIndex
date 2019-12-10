5d2eeddef2be04c5a3038a724e77e8d0c19563ec
YourFin <d.junkpobox+git@gmail.com>
2019-12-09 22:11:17 -0800

    Add semicolons

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
-const { environment } = require('@rails/webpacker')
-const elm =  require('./loaders/elm')
+const { environment } = require('@rails/webpacker');
+const elm =  require('./loaders/elm');
 
-environment.loaders.prepend('elm', elm)
-module.exports = environment
+environment.loaders.prepend('elm', elm);
+module.exports = environment;
