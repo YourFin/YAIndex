e49ed28c6688b0c0e359adaa8b3c4514c0acc74c
YourFin <d.junkpobox+git@gmail.com>
2019-12-18 13:26:19 -0800

    Fix lint errors

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,9 @@
 class RequestsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     render plain: params[:request].inspect
   end
 
-  def list
-  end
+  def list; end
 end
--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class RequestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get requests_new_url
     assert_response :success
   end
 
-  test "should get list" do
+  test 'should get list' do
     get requests_list_url
     assert_response :success
   end
-
 end
