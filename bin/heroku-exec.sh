8361c480c059dae5b60a0169fd905fc3727dacc6
YourFin <d.junkpobox+git@gmail.com>
2020-04-01 19:25:04 -0700

    Better heroku-exec.sh?

--- bin/heroku-exec.sh
+++ bin/heroku-exec.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
 # Script to allow heroku remote debugging
-[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
+set -eou pipefail
+
+[ -z "$SSH_CLIENT" ] || exit 0
+temp_file="$(mktemp)"
+cleanup () {
+    rm "$temp_file"
+}
+curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL" > "$temp_file"
+# shellcheck disable=1090
+. "$temp_file"
+echo "Set up heroku remote server successfully!"
