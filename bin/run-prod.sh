b1d67323d316593a1cb52e5ae4f128ad987153b0
YourFin <d.junkpobox+git@gmail.com>
2020-04-01 18:52:51 -0700

    Nicer output from production docker container

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -18,17 +18,22 @@ if [[ "${FILES_DIRECTORY: -1}" != "/" ]] ; then
     export FILES_DIRECTORY="$FILES_DIRECTORY/"
 fi
 
+NGINX_CONFIG="/opt/yaindex/nginx/nginx-prod.conf"
+
+echo "Creating nginx config from template..."
 # Substitute environment variables into nginx.conf
 # shellcheck disable=SC2016
-envsubst '$PORT $FILES_DIRECTORY' < nginx/nginx.template.conf > nginx/nginx-prod.conf
+envsubst '$PORT $FILES_DIRECTORY' < nginx/nginx.template.conf > "$NGINX_CONFIG"
 
 # Start nginx
-nginx -c /opt/yaindex/nginx/nginx-prod.conf
+echo "Starting nginx..."
+nginx -c "$NGINX_CONFIG"
 
 # Kill nginx when this script stops
 cleanup () {
-    nginx -c  /opt/yaindex/nginx/nginx-prod.conf -s quit
+    nginx -c "$NGINX_CONFIG"
 }
 trap cleanup EXIT
 
-sleep infinity
+echo "Reading from nginx error log:"
+tail -f /var/log/nginx/error.log
