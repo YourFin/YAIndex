fe7b7b5f35989ba91d77668414217a0c5a75ce91
YourFin <d.junkpobox+git@gmail.com>
2020-04-04 21:43:48 -0700

    Simplify goals for video

--- elm-src/ContentType/Video.elm
+++ elm-src/ContentType/Video.elm
@@ -1,28 +1,68 @@
 module ContentType.Video exposing (..)
 
 import Array exposing (Array)
-import ContentType.Video.AudioTrack as AudioTrack exposing (AudioTrack)
 import ContentType.Video.MediaTime as MediaTime exposing (MediaTime)
+import Html exposing (Html)
+import Html.Attributes as Attr
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode exposing (Value)
+import Routing exposing (ContentId)
+
+
+
+-- MODEL
 
 
 type alias Model =
     { currentTime : MediaTime
-    , muted : Bool
     , volume : Float
-    , duration : MediaTime
-    , videoTrackIdx : Int
-    , audioTrackIdx : Int
-    , subTrackIdx : Maybe Int
+    , duration : Maybe MediaTime
+    , ephemerals : Ephemerals
     }
 
 
-{-| Internal video bits that are /not/ saved
--}
+empty : Model
+empty =
+    { currentTime = MediaTime.fromFloat 0
+    , volume = 1
+    , duration = Nothing
+    , ephemerals = {}
+    }
+
+
 type alias Ephemerals =
-    { audioTracks : Array AudioTrack
-    , videoTracks : () -- Probably a dict?
-    , subTracks : ()
-    , playing : Bool
-    }
+    {}
+
+
+
+-- UPDATE
+
+
+type Message
+    = TimeUpdate MediaTime
+    | GotLength MediaTime
+    | VolumeChanged Float
+
+
+update : Message -> Model -> ( Model, Cmd msg )
+update msg model =
+    case msg of
+        TimeUpdate time ->
+            ( { model | currentTime = time }, Cmd.none )
+
+        VolumeChanged vol ->
+            ( { model | volume = vol }, Cmd.none )
+
+        GotLength length ->
+            ( { model | duration = Just length }, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Routing.Roots -> ContentId -> Model -> Html Message
+view roots contentId model =
+    Html.video [ Attr.src <| Routing.rawUrl roots contentId ]
+        [ Html.text "Could not load video"
+        ]
